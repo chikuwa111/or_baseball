bfbc592d414ca5f32fe24e141ed693aad33cf5f0
chikuwa111 <take0shun1111@gmail.com>
2016-12-08 12:03:34 +0900

    toggle detail show

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,18 @@
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require_tree .
+
+$(function() {
+  $(".detail-btn").click(function() {
+    $(this).parent().children(".detail").slideToggle();
+    if ($(this).children(".fa").hasClass("fa-chevron-down")) {
+      $(this).children(".fa").removeClass("fa-chevron-down");
+      $(this).children(".fa").addClass("fa-chevron-up");
+      $(this).children(".gap").text("詳細を隠す");
+    } else {
+      $(this).children(".fa").removeClass("fa-chevron-up");
+      $(this).children(".fa").addClass("fa-chevron-down");
+      $(this).children(".gap").text("詳細を見る");
+    }
+  })
+});
